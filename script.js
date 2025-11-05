d3c66c1e8322554df698742e448b8a0bc8744961
ivanchor <ichor85@gmail.com>
2025-11-04 16:22:51 -0800

    Add Eventlistener to grid squares
    Will change the color of the square to black and white

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ function createGrid(size){
     for(i = 0; i < size*size; i++){
         const box = document.createElement(`div`)
         box.className = "squares"
+        box.style.backgroundColor = 'white'
         box.style.width = `${box_width}px`;
         box.style.height = `${box_height}px`;
         // box.style.width=`calc(1000px/${size})`;
         // box.style.height=`calc(1000px/${size})`;
+
+
+        box.addEventListener('mouseover', () => {
+            if(box.style.backgroundColor == 'white')
+                box.style.backgroundColor = 'black';
+            else
+                box.style.backgroundColor = 'white';
+            
+        })
+
         gridContainer.appendChild(box);
         
     }
